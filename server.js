da45f74a0a9bde8e64a3dd24b3946bd3a600bdf4
bloo0178 <bloo0178@gmail.com>
2018-09-20 18:01:19 -0400

    092018-1801

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+})
+var upload = multer({storage: storage}).single('upfile');
 
 var app = express();
 
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -18,6 +29,27 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+    }
+  })
+  
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
+
+/*
+  <form enctype="multipart/form-data" method="POST" action="/api/fileanalyse">
+            <input id="inputfield" type="file" name="upfile">
+            <input id="button" type="submit" value="Upload">
+            
+
+  {"name":"P4 Log.png","type":"image/png","size":16214}
+*/
